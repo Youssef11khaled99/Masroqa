07ad3bb2d3a4e113e1dfb252f1ebdf7d2e093c5f
MlHisham <catchm.hisham@gmail.com>
2018-12-23 01:56:28 +0200

    now register fully functional using database name masroqa,table user and attribute username email password

--- Backend/UserAccount.php
+++ Backend/UserAccount.php
@@ -1,19 +1,63 @@
-<?php
+<?php	
 	class UserAccount{
 		private $userName;
 		private $emailAddress;
 		private $description;
 		private $password;
-		function __construct($userName,$emailAddress,$description,$password){
-			$this->$userName = $userName;
-			$this->$emailAddress = $emailAddress;
-			$this->$description = $description ;
-			$this->$password = $password;
-		}
+		
 		public function login(){
 		}
 		public function signUp(){
-			
+				session_start();
+				$userName = "";
+				$emailAddress = "";
+				$errors = array(); 
+				// connect to the database
+				$db = mysqli_connect('localhost', 'root', '', 'masroqa');
+				// REGISTER USER
+				if (isset($_POST['reg_user'])) {
+				  // receive all input values from the form
+				  $userName = mysqli_real_escape_string($db, $_POST['username']);
+				  $emailAddress = mysqli_real_escape_string($db, $_POST['email']);
+				  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+				  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+				  // form validation: ensure that the form is correctly filled ...
+				  // by adding (array_push()) corresponding error unto $errors array
+				  if (empty($userName)) { array_push($errors, "username is required"); }
+				  if (empty($emailAddress)) { array_push($errors, "email is required"); }
+				  if (!filter_var($emailAddress, FILTER_VALIDATE_EMAIL)) {
+  						array_push($errors, "Invalid email format"); 
+					}
+				  if (empty($password_1)) { array_push($errors, "Password is required"); }
+				  if ($password_1 != $password_2) {
+					array_push($errors, "The two passwords do not match");
+				  }
+				  // first check the database to make sure 
+				  // a user does not already exist with the same userName and/or emailAddress
+				  $user_check_query =  "SELECT * FROM user WHERE username='$userName' OR email='$emailAddress' LIMIT 1";
+				  $result = mysqli_query($db, $user_check_query);
+				  $user = mysqli_fetch_assoc($result);
+				  if ($user) { // if user exists
+				    if ($user['username'] === $userName) {
+				      array_push($errors, "username already exists");
+				    }
+				    if ($user['email'] === $emailAddress) {
+				      array_push($errors, "email already exists");
+				    }
+				  }
+				  // Finally, register user if there are no errors in the form
+				  if (count($errors) == 0) {
+				  	$password = md5($password_1);//encrypt the password before saving in the database
+
+				  	$query = "INSERT INTO user (username, email, password) 
+				  			  VALUES('$userName', '$emailAddress', '$password')";
+				  	mysqli_query($db, $query);
+				  	$_SESSION['username'] = $userName;
+				  	$_SESSION['success'] = "You are now logged in";
+				  	header('location: ../index.php');
+				  }
+				}
+				return $errors;
 		}
 		public function editInfo(){
 		}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+	session_start(); 
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>Masroqa</title>
+  <!-- this is the main file don't edit now edit html pages only!!-->
+</head>
+<body>
+	<h1>Homepage</h1>
+	<?php
+		if (isset($_SESSION['username'])) {
+	  ?>
+	  	<a href= "html/post.html">Create post</a>
+		<a href= "Backend/personalProfile.php"><?php echo $_SESSION['username'] ?></a>
+	<?php }
+		else{
+	?>
+		<a href= "html/login.html">Login</a>
+		<a href= "Backend/signup.php">Signup</a>
+	<?php }?>
+</body>
+</html>
