986a6b65d81f7de5ef23ceec0b84e70f85dcf292
nerdo <walieda74@gmail.com>
2018-12-24 14:14:35 +0200

    Trying to work on database but some issued are ocurring.

--- Backend/SearchControl.php
+++ Backend/SearchControl.php
@@ -5,7 +5,7 @@ $locationItem = $_POST["li"];
 
 
 
-
+echo "Hello. <br/>";
 
 echo 'Your descripton is: '.$tArea.'<br/> Your object type is: '.$objectType.'<br/>Item
 of the location is: '.$locationItem.'<br/>';
@@ -19,19 +19,33 @@ of the location is: '.$locationItem.'<br/>';
 $servername = "localhost";
 $username = "nerdo";
 $password = "22963490nerdoO@";
+
 // Create connection
-$conn = mysqli_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
 
 
 
 /*Searches in database*/
+$sql = "SELECT location FROM post";//A problem here.
+$result = $conn->query($sql);
+echo "Result of query: ".$result."<br/>";
 
+echo "no. of rows: ".$result->num_rows."<br/>";
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "photopath: " . $row["photoPath"]. " - DOL " . $row["DOL"]. " " . $row["numOfReports"]. "<br>";
+    }
+} else {
+    echo "0 results";
+    echo "'";
+}
 
 
 
